b54541356dcb9178b5e31ab31ce164490ff2a15e
Pierre <prange@quarzwerke.com>
2020-06-01 13:32:00 +0200

    fixed header already sent

--- postMessage.php
+++ postMessage.php
@@ -1,14 +1,9 @@
 <?php
 session_start();
-
-
 $fk_gruppe = $_SESSION['GID'];
 $message = $_POST['message'];
 $fk_autor = $_SESSION['UID'];
-
 $db = new SQLite3 ('production.sqlite');
-$db->query("insert into nachricht (nachrichtentext, fk_autor, fk_gruppe, sent_at) 
-VALUES ('". $message . "', " . $fk_autor . ", " . $fk_gruppe . ", CURRENT_TIMESTAMP )");
-
+$db->query("insert into nachricht (nachrichtentext, fk_autor, fk_gruppe, sent_at) VALUES ('". $message . "', " . $fk_autor . ", " . $fk_gruppe . ", CURRENT_TIMESTAMP )");
 header("Location: view.php");
 ?>
